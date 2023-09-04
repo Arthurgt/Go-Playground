40f6071148aa94c01aaa061bcddd6f4b4d5ed595
Arthurgt <arturbabkiewicz@gmail.com>
2023-09-04 23:06:20 +0200

    Commit_6 - Created channels

---
+++ channels.go
@@ -0,0 +1,15 @@
+package main
+
+func makeChannels() {
+	links := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://golang.org",
+		"http://amazon.com",
+		"http://stackoverflow.com",
+	}
+
+	for _,link := range links {
+		
+	}
+}
